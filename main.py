71047526e042be8225c497f3ddbb703eeab4cca9
Ermaotie <Meise1015@gmail.com>
2020-06-10 21:51:41 +0800

    Update main.py
    main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# import werobot
+import werobot
 import requests as req
 import json
 
 
-# robot = werobot.WeRoBot(token='tokenhere')
+robot = werobot.WeRoBot(token='tokenhere')
 api = "http://a5.zimingdh.com/jiekou/api.php?question="
 robot.config["APP_ID"] = "wx4946d787a25bc3d4"
 robot.config["APP_SECRET"] = "wx4946d787a25bc3d4"
-# @robot.handler
+@robot.handler
 def echo(message):
     request = req.get(api+message, verify=False, timeout=30)
     request.encoding = 'utf-8'
@@ -15,7 +15,7 @@ def echo(message):
     return "问："+answer["question"]+"\\n"+"答："+answer["answer"]
 
 
-# robot.config['HOST'] = '0.0.0.0'
-# robot.config['PORT'] = 80
-print(echo("中华人民共和国成立时间"))
-# robot.run()
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 80
+# print(echo("中华人民共和国成立时间"))
+robot.run()
