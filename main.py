4f0b0e869700ba09efba1881e2d250c4b41fe944
Ermaotie <meise1015@gmail.com>
2020-08-04 15:43:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ robot.config["APP_SECRET"] = "742523c2e05cc02d9ebf66ebffa8a9f6"
 
 @robot.image
 def img(message):
-    return search_demo(message.img)
+    return search_demo(message.image)
 
 
 robot.config['HOST'] = '0.0.0.0'
