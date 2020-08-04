a61d907d280b04951079a6dcff12b3d958bf6bbe
Ermaotie <meise1015@gmail.com>
2020-08-04 15:27:31 +0800

    update function

---
+++ function.py
@@ -0,0 +1,39 @@
+from aip import *
+
+INFO = ['21786513',  # APP id
+        '6RW8ATqt8XVsV2wNSnwbYMfw',  # key
+        'HGjjFpAeqrqibnoIiFdQKGeNFbKTAOGk',  # secret key
+        ]
+
+options = {
+    'baike_num': 1,
+}
+classifyClient = AipImageClassify(*INFO)
+searchClient = AipImageSearch(*INFO)
+
+
+def info_format(result):
+    keyword = result['keyword']
+    description = result['baike_info']['description']
+    image_url = result['baike_info']['image_url']
+    res = '查询结果：' + '\n' + keyword + '\n' + '百度百科:' + '\n' + description + '\n' + '图片链接:' + '\n' + image_url
+    return res
+
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
+def classify_demo(image):
+    res = classifyClient.advancedGeneral(image, options)
+    return info_format(res['result'][0])
+
+
+def search_demo(image):
+    res = searchClient.productSearch(image, options)
+    return info_format(res['result'][0])
+
+
+image = get_file_content('D://临时/1584152777_458425.jpg')
+print(classify_demo(image))
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #coding=utf-8
 import werobot
-import requests as req
-import json
-from find_api import *
+from function import *
 
 
 robot = werobot.WeRoBot(token='tokenhere')
 robot.config["APP_ID"] = "wx4946d787a25bc3d4"
 robot.config["APP_SECRET"] = "742523c2e05cc02d9ebf66ebffa8a9f6"
-@robot.handler
-def echo(message):
-    demo = DA(message.content)
-    return "问："+demo.getQuestion()+"\n"+"答："+demo.getAnswer()
+
+@robot.image
+def img(message):
+    return search_demo(message.img)
 
 
 robot.config['HOST'] = '0.0.0.0'
