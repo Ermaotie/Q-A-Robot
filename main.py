e5574dfc8c870ba50ff7132f005219f28dca7a5f
Ermaotie <meise1015@gmail.com>
2020-08-07 23:25:32 +0800

    add the subscribe.py

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 #coding=utf-8
 import werobot
 from function import *
+import requests
 
 
 robot = werobot.WeRoBot(token='tokenhere')
 robot.config["APP_ID"] = "wx4946d787a25bc3d4"
 robot.config["APP_SECRET"] = "742523c2e05cc02d9ebf66ebffa8a9f6"
 
+url = 'http://sc.ftqq.com/3.version'
+api = 'http://sc.ftqq.com/SCKEY.send?text='
+
+
 @robot.image
 def img(message):
     return classify_demo(message.img)
 
 
+@robot.filter('订阅通知')
+def subscribe():
+    return '在微信绑定Server酱之后，请将SCKEY发送至本公众号\n格式为：\nSCKEY xxxxxx\n如果您不知道什么是SCKEY，请阅读'+url
+
+
+@robot.filter('SCKEY' or 'sckey')
+def sub(message):
+    key = message.content.split()[-1]
+    file = open('./SCKEYS.txt', 'a', encoding='UTF-8')
+    file.write(key+'\n')
+    file.close()
+    requests.get(api + '恭喜绑定成功，请等待推送')
+    return '请查看消息列表'
+
+
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 80
-robot.run()
+# robot.run()
---
+++ subscribe.py
@@ -0,0 +1,67 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+url = "http://jw.scut.edu.cn/zhinan/cms/article/v2/findInformNotice.do"
+baseLink = 'http://jw.scut.edu.cn/zhinan/cms/article/view.do?type=posts&id='
+payload = 'category=0&tag=0&pageNum=1&pageSize=15&keyword='
+headers = {
+  'Referer': 'http://jw.scut.edu.cn/zhinan/cms/toPosts.do',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Cookie': 'JSESSIONID=44A66ED72B96AEE63A9EB9938187871F; clwz_blc_pst_JWCx2djw=4211753326.20480'
+}
+ft_url = 'https://sc.ftqq.com/'
+ft_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+
+
+
+
+def getJsons():
+    response = requests.request("POST", url, headers=headers, data = payload)
+    response = json.loads(response.text)['list']
+    return response[0:2]
+
+
+def getDesp(id,baselink):
+    articleUrl = baselink+id
+    request = requests.get(articleUrl)
+    request.encoding = 'utf-8'
+    soup = BeautifulSoup(request.text,'html.parser')
+    soup = soup.select_one('div.content')
+    content = ''
+    for string in soup.strings:
+        content = content + string
+    if len(content)>900:
+        return content[0:200] + '...'+'\n'+"\n" + '原文地址：\n' + articleUrl
+    else:
+        return content
+
+
+def getINFO():
+    lists = getJsons()
+    file = open('./Titles.txt', 'r', encoding='UTF-8')
+    lastlist = file.readline()
+    file.close()
+    if lists is not lastlist:
+        file = open('./Titles.txt', 'w+', encoding='UTF-8')
+        file.write(lists)
+        file.close()
+        id = lists[0]['id']
+        time = lists[0]['createtime']
+        title = lists[0]['title']
+
+        text = time +' ' + title
+        desp = getDesp(id,baseLink)
+        data = {'text': text, 'desp':desp}
+        sFile = open('./SCKEYS.txt','r',encoding='utf-8')
+        SCKEYS = sFile.readlines()
+        sFile.close()
+
+        send(SCKEYS,data)
+
+
+def send(SCKEYS,data):
+    for SCKEY in SCKEYS:
+        eachurl = ft_url + SCKEY + '.send'
+        requests.post(eachurl, data, headers=ft_headers)
