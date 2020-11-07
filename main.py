7bd5eacab7a71757ea0e947a652c59f86b429afb
Ermaotie <meise1015@gmail.com>
2020-11-07 23:37:01 +0800

    finish the subcription

--- main.py
+++ main.py
@@ -1,51 +1,16 @@
 #coding=utf-8
 import werobot
-from function import *
-import requests
 
 
 robot = werobot.WeRoBot(token='tokenhere')
-robot.config["APP_ID"] = "wx4946d787a25bc3d4"
-robot.config["APP_SECRET"] = "742523c2e05cc02d9ebf66ebffa8a9f6"
-
-url = 'http://sc.ftqq.com/3.version'
-api = 'http://sc.ftqq.com/'
-
-
-@robot.image
-def img(message):
-    return classify_demo(message.img)
-
-
-@robot.filter('订阅通知')
-def subscribe():
-    return '在微信绑定Server酱之后，请将SCKEY发送至本公众号。\n目前共有两种模式：\n1.实时推送\n格式为：\nSCKEY xxxxxx\n2.每日推送\n格式为：\nSCKEY 1 xxxxxxx\n如果您还未绑定或不知道什么是SCKEY，请返回教程继续阅读'+'https://1b.mk/2020/08/08/subscribe/'
+robot.config["APP_ID"] = "wx7407f5c28abc23c0"
+robot.config["APP_SECRET"] = "0b0e1bf34d4253ff1e550830da0818f8"
 
 
 @robot.text
 def sub(message):
-    if 'SCKEY' in message.content:
-        msg = message.content.split(' ')
-        key = msg[-1] + '\n'
-        file = open('./SCKEYS.txt', 'r+', encoding='UTF-8')
-        keys = file.readlines()
-        print(msg)
-        if '1 '+key in keys:
-            file.close()
-            requests.get(api + msg[-1] + '.send?text=' + '已订阅成功，请等待推送')
-            return '你已经成功添加订阅了每日订阅，无需重复添加~查看消息列表以检查，若无消息，请尝试重新绑定'
-        if '0 ' + key in keys:
-            file.close()
-            requests.get(api + msg[-1] + '.send?text=' + '已订阅成功，请等待推送')
-            return '你已经成功添加订阅了实时订阅，无需重复添加~查看消息列表以检查，若无消息，请尝试重新绑定'
-        if msg[1]=='1' or msg[1] == '按日推送':
-            key = '1 '+ key
-        else:
-            key = '0 '+key
-        file.write(key)
-        file.close()
-        requests.get(api + msg[-1] + '.send?text=' + '恭喜订阅成功，请等待推送')
-        return '请查看消息列表,是否有示例消息发送；若无，则请登陆 https://sc.ftqq.com/ 检查是否已绑定Server酱'
+    return message.content
+
 
 @robot.subscribe
 def subscribe():
@@ -56,3 +21,6 @@ def subscribe():
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 80
 robot.run()
+
+
+
